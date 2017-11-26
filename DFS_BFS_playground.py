963be8502867f50f976dc7bdacf16969fb7434ed
alexander-ray <1alexray@gmail.com>
2017-11-26 13:16:48 -0700

    Added iterative DFS and BFS

--- DFS_BFS_playground.py
+++ DFS_BFS_playground.py
@@ -1,6 +1,12 @@
+import queue
+
 class Graph:
     def __init__(self, nodes):
         self.nodes = nodes
+
+    def reset(self):
+        for i in self.nodes:
+            i.visited = False
 
 class Node:
     def __init__(self, id):
@@ -11,6 +17,7 @@ class Node:
     def add_neighbor(self, node):
         self.neighbors.append(node)
 
+# Recursive version of DFS
 def dfs_recursive(root):
     if (root == None):
         return
@@ -21,12 +28,58 @@ def dfs_recursive(root):
         if (i.visited == False):
             dfs_recursive(i)
 
-def search_helper(graph):
-    dfs_recursive(graph.nodes[0])
+# Iterative version of DFS
+# Uses stack as primary data structure
+def dfs_iterative(root):
+    q = queue.LifoQueue()
 
-    # Reset graph
-    for i in graph.nodes:
-        i.visited = False
+    # Visit root and add to queue
+    root.visited = True
+    q.put(root)
+
+    while not q.empty():
+        # Get node at front of queue, visit
+        curr = q.get()
+        print("Node: " + str(curr.id))
+        # Iterate through neighbors, visiting as necessary
+        # Add each neighbor to queue if not visited
+        for i in curr.neighbors:
+            if not i.visited:
+                i.visited = True
+                q.put(i)
+
+# BFS
+# Uses FIFO queue as primary data structure
+def bfs(root):
+    q = queue.Queue()
+
+    # Visit root and add to queue
+    root.visited = True
+    q.put(root)
+
+    while not q.empty():
+        # Get node at front of queue, visit
+        curr = q.get()
+        print("Node: " + str(curr.id))
+        # Iterate through neighbors, visiting as necessary
+        # Add each neighbor to queue if not visited
+        for i in curr.neighbors:
+            if not i.visited:
+                i.visited = True
+                q.put(i)
+
+def search_helper(graph):
+    print("DFS Recursive:")
+    dfs_recursive(graph.nodes[0])
+    graph.reset()
+
+    print("DFS Iterative:")
+    dfs_iterative(graph.nodes[0])
+    graph.reset()
+
+    print("BFS:")
+    bfs(graph.nodes[0])
+    graph.reset()
 
 def main():
    graph = Graph([Node(x) for x in range(0, 6)])
