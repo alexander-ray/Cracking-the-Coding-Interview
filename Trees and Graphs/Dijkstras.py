9a5f38a9b4512433b77222a367fa0d3796274531
alexander-ray <1alexray@gmail.com>
2017-12-02 10:27:32 -0700

    Fixed dijkstras

--- Trees and Graphs/Dijkstras.py
+++ Trees and Graphs/Dijkstras.py
@@ -1,5 +1,5 @@
 import math
-import queue
+import heapq
 
 class Graph:
     def __init__(self, nodes):
@@ -14,8 +14,26 @@ class Node:
         self.neighbors.append((node, weight))
 
 
-def dijkstras(g, target_id):
-    print('hello')
+def dijkstras(g, start_id, target_id):
+    remaining = []
+    path_weight = [math.inf for _ in g]
+    previous = [-1 for _ in g]
+
+    path_weight[start_id] = 0
+    heapq.heappush(remaining, (0, g[start_id].id, g[start_id]))
+
+    while remaining:
+        w, _, n = heapq.heappop(remaining)
+        path_weight[n.id] = w
+        previous[n.id] = n.id
+
+        if n.id == target_id:
+            return path_weight[n.id]
+
+        for e, w2 in n.neighbors:
+            if math.isinf(path_weight[e.id]):
+                heapq.heappush(remaining, (path_weight[n.id] + w2, e.id ,e))
+    return float("inf")
 
 def main():
     graph = Graph([Node(x) for x in range(0, 6)])
@@ -23,12 +41,14 @@ def main():
     g[0].add_neighbor(g[1], 2)
     g[0].add_neighbor(g[4], 1)
     g[0].add_neighbor(g[5], 1)
-    g[1].add_neighbor(g[3], 5)
+    g[1].add_neighbor(g[3], 10)
     g[1].add_neighbor(g[4], 3)
     g[2].add_neighbor(g[1], 1)
     g[3].add_neighbor(g[2], 4)
     g[3].add_neighbor(g[4], 3)
+    g[5].add_neighbor(g[3], 2)
 
+    print(dijkstras(g, 0, 3))
 
 
 if __name__ == '__main__':
